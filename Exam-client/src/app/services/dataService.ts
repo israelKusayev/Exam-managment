ee02faafab3a4f5f5c95e3ef2bc43fb8513dcf0e
ישראל קוסייב <israel.kusayev@gmail.com>
2019-02-02 18:59:05 +0200

    added data service and common exceptions

---
+++ Exam-client/src/app/services/dataService.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { NotFoundError } from '../exceptions/not-found-error';
+import { AppError } from '../exceptions/app-error';
+import { BadInput } from '../exceptions/bad-input';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  constructor(private url: string, private http: HttpClient) {}
+
+  getAll() {
+    return this.http
+      .get<any[]>(this.url)
+      .pipe(catchError((error: Response, caught) => this.handleError(error)));
+  }
+
+  create(resource) {
+    return this.http
+      .post(this.url + '/', JSON.stringify(resource))
+      .pipe(catchError((error: Response, caught) => this.handleError(error)));
+  }
+
+  update(resource) {
+    return this.http
+      .patch(this.url + '/' + resource.id, JSON.stringify(resource))
+      .pipe(catchError((error: Response, caught) => this.handleError(error)));
+  }
+
+  delete(id) {
+    return this.http
+      .delete(this.url + 'pp/' + id)
+      .pipe(catchError((error: Response, caught) => this.handleError(error)));
+  }
+
+  private handleError(error: Response) {
+    if (error.status === 404) {
+      return throwError(new NotFoundError());
+    }
+    if (error.status === 400) {
+      return throwError(new BadInput());
+    }
+    return throwError(new AppError(error));
+  }
+}
